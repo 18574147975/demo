a015e83037519a1613b79da7146202ea3afc768c
shanmu <zhenhua7975@163.com>
2025-11-14 22:28:28 +0800

    Fix: 修复刷新页面 404 问题

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/choice-helper/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/demo/' : '/',
   plugins: [
     react(),
     miaodaDevPlugin(),
